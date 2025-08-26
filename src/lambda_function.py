ceb946900b2f2e7d6f762798fdece99b35ce2bff
sampathkmanchala <60176602+sampathkmanchala@users.noreply.github.com>
2025-08-26 19:37:44 +0530

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,5 +2,7 @@ import requests
 from pyqrcode import create as create_qr_code 
 
 def handler2(event,context):
+  result = requests.get("www.google.com")
+  print("result", result)
   return "Hello World2"
 
